1cfc4fb52c38742a9b65eda31bad82edf21f8d11
ljianf <ljianf@servyou.local>
2017-09-30 18:02:07 +0800

    cloud

---
+++ microservice-simple-provider-usre/microservice-simple-provider-usre/src/main/java/com/itmuch/cloud/microservicesimpleproviderusre/MicroserviceSimpleProviderUsreApplication.java
@@ -0,0 +1,12 @@
+package com.itmuch.cloud.microservicesimpleproviderusre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSimpleProviderUsreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSimpleProviderUsreApplication.class, args);
+	}
+}
